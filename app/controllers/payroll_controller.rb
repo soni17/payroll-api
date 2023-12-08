00db2ade7d85a3de75cbf4c0251859cde219ffc5
Enderson Isai <elb004@gmail.com>
2023-12-08 14:03:36 -0500

    add file upload to payroll controller

--- app/controllers/payroll_controller.rb
+++ app/controllers/payroll_controller.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'active_support/core_ext'
+
 class PayrollController < ApplicationController
   skip_before_action :verify_authenticity_token
 
@@ -6,9 +9,74 @@ class PayrollController < ApplicationController
   end
 
   def upload_timesheet
-    render plain: "test"
+    timesheet = Timesheet.find_by(sheet_id: sheet_id)
+
+    if timesheet
+      render status: 500, json: {status: "error", message: "There's already a timesheet with id #{sheet_id}"}
+    else
+      process_file_upload
+      render json: {status: "ok", message: "File content uploaded"}
+    end
   end
 
   def report
+  end
+
+  private
+
+  def file
+    params['file']
+  end
+
+  def filename
+    file.original_filename
+  end
+
+  def sheet_id
+    filename.split('-').last.split('.').first
+  end
+
+  def file_content
+    content = CSV.read(file)
+    content.shift # remove first element
+    content
+  end
+
+  def process_file_upload
+    timesheet = Timesheet.create!(filename: filename, sheet_id: sheet_id)
+
+    file_content.each do |row|
+      pay_period = create_pay_period(row[0],row[2])
+      LineItem.create!(
+        date: Date.parse(row[0]),
+        hours_worked: row[1],
+        employee_id: row[2],
+        job_group: row[3],
+        timesheet: timesheet,
+        pay_period: pay_period
+      )
+    end
+  end
+  
+  def create_pay_period(date,employee_id)
+    date = Date.parse(date)
+    start_date = get_start_date(date)
+    end_date = get_end_date(date)
+    
+    PayPeriod.find_or_create_by!(
+      start_date: start_date,
+      end_date: end_date,
+      employee_id: employee_id
+    )
+  end
+
+  def get_start_date(date)
+    start_date = date.day < 16 ? 1 : 16
+    Date.new(date.year, date.month, start_date)
+  end
+
+  def get_end_date(date)
+    end_date = date.day < 16 ? 15 : date.end_of_month.day
+    Date.new(date.year, date.month, end_date)
   end
 end
